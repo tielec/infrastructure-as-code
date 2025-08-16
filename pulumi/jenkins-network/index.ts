0b77c0c7d57fb448b8a4e7a371e23593433a99f7
YutoTakashi <yuto.takashi@plan-b.co.jp>
2025-08-16 14:38:29 +0900

    add ssm parameter initialize role

--- pulumi/jenkins-network/index.ts
+++ pulumi/jenkins-network/index.ts
@@ -7,26 +7,36 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// コンフィグから設定を取得
-const config = new pulumi.Config();
-const projectName = config.get("projectName") || "jenkins-infra";
-const vpcCidrBlock = config.get("vpcCidr") || "10.0.0.0/16";
+// 環境名をスタック名から取得
 const environment = pulumi.getStack();
+const ssmPrefix = `/jenkins-infra/${environment}`;
+
+// SSMパラメータから設定を取得
+const projectNameParam = aws.ssm.getParameter({
+    name: `${ssmPrefix}/config/project-name`,
+});
+const vpcCidrParam = aws.ssm.getParameter({
+    name: `${ssmPrefix}/config/vpc-cidr`,
+});
+
+// 設定値を変数に設定
+const projectName = pulumi.output(projectNameParam).apply(p => p.value);
+const vpcCidrBlock = pulumi.output(vpcCidrParam).apply(p => p.value);
 
 // VPC作成
-const vpc = new aws.ec2.Vpc(`${projectName}-vpc`, {
+const vpc = new aws.ec2.Vpc(`vpc`, {
     cidrBlock: vpcCidrBlock,
     enableDnsHostnames: true,
     enableDnsSupport: true,
     tags: {
-        Name: `${projectName}-vpc-${environment}`,
+        Name: pulumi.interpolate`${projectName}-vpc-${environment}`,
         Environment: environment,
         ManagedBy: "pulumi",
     },
 });
 
 // インターネットゲートウェイ
-const igw = new aws.ec2.InternetGateway(`${projectName}-igw`, {
+const igw = new aws.ec2.InternetGateway(`igw`, {
     vpcId: vpc.id,
     tags: {
         Name: `${projectName}-igw-${environment}`,
@@ -38,7 +48,7 @@ const igw = new aws.ec2.InternetGateway(`${projectName}-igw`, {
 const azs = pulumi.output(aws.getAvailabilityZones({}));
 
 // パブリックサブネットA
-const publicSubnetA = new aws.ec2.Subnet(`${projectName}-public-subnet-a`, {
+const publicSubnetA = new aws.ec2.Subnet(`public-subnet-a`, {
     vpcId: vpc.id,
     cidrBlock: "10.0.0.0/24",
     availabilityZone: azs.names[0],
@@ -51,7 +61,7 @@ const publicSubnetA = new aws.ec2.Subnet(`${projectName}-public-subnet-a`, {
 });
 
 // パブリックサブネットB
-const publicSubnetB = new aws.ec2.Subnet(`${projectName}-public-subnet-b`, {
+const publicSubnetB = new aws.ec2.Subnet(`public-subnet-b`, {
     vpcId: vpc.id,
     cidrBlock: "10.0.2.0/24",
     availabilityZone: azs.names[1],
@@ -64,7 +74,7 @@ const publicSubnetB = new aws.ec2.Subnet(`${projectName}-public-subnet-b`, {
 });
 
 // プライベートサブネットA
-const privateSubnetA = new aws.ec2.Subnet(`${projectName}-private-subnet-a`, {
+const privateSubnetA = new aws.ec2.Subnet(`private-subnet-a`, {
     vpcId: vpc.id,
     cidrBlock: "10.0.1.0/24",
     availabilityZone: azs.names[0],
@@ -76,7 +86,7 @@ const privateSubnetA = new aws.ec2.Subnet(`${projectName}-private-subnet-a`, {
 });
 
 // プライベートサブネットB
-const privateSubnetB = new aws.ec2.Subnet(`${projectName}-private-subnet-b`, {
+const privateSubnetB = new aws.ec2.Subnet(`private-subnet-b`, {
     vpcId: vpc.id,
     cidrBlock: "10.0.3.0/24",
     availabilityZone: azs.names[1],
@@ -88,7 +98,7 @@ const privateSubnetB = new aws.ec2.Subnet(`${projectName}-private-subnet-b`, {
 });
 
 // パブリックルートテーブル
-const publicRouteTable = new aws.ec2.RouteTable(`${projectName}-public-rt`, {
+const publicRouteTable = new aws.ec2.RouteTable(`public-rt`, {
     vpcId: vpc.id,
     tags: {
         Name: `${projectName}-public-rt-${environment}`,
@@ -97,26 +107,26 @@ const publicRouteTable = new aws.ec2.RouteTable(`${projectName}-public-rt`, {
 });
 
 // パブリックルート（インターネットゲートウェイ向け）
-const publicRoute = new aws.ec2.Route(`${projectName}-public-route`, {
+const publicRoute = new aws.ec2.Route(`public-route`, {
     routeTableId: publicRouteTable.id,
     destinationCidrBlock: "0.0.0.0/0",
     gatewayId: igw.id,
 });
 
 // パブリックサブネットAとルートテーブルの関連付け
-const publicRtAssociationA = new aws.ec2.RouteTableAssociation(`${projectName}-public-rta-a`, {
+const publicRtAssociationA = new aws.ec2.RouteTableAssociation(`public-rta-a`, {
     subnetId: publicSubnetA.id,
     routeTableId: publicRouteTable.id,
 });
 
 // パブリックサブネットBとルートテーブルの関連付け
-const publicRtAssociationB = new aws.ec2.RouteTableAssociation(`${projectName}-public-rta-b`, {
+const publicRtAssociationB = new aws.ec2.RouteTableAssociation(`public-rta-b`, {
     subnetId: publicSubnetB.id,
     routeTableId: publicRouteTable.id,
 });
 
 // プライベートルートテーブルA
-const privateRouteTableA = new aws.ec2.RouteTable(`${projectName}-private-rt-a`, {
+const privateRouteTableA = new aws.ec2.RouteTable(`private-rt-a`, {
     vpcId: vpc.id,
     tags: {
         Name: `${projectName}-private-rt-a-${environment}`,
@@ -125,7 +135,7 @@ const privateRouteTableA = new aws.ec2.RouteTable(`${projectName}-private-rt-a`,
 });
 
 // プライベートルートテーブルB
-const privateRouteTableB = new aws.ec2.RouteTable(`${projectName}-private-rt-b`, {
+const privateRouteTableB = new aws.ec2.RouteTable(`private-rt-b`, {
     vpcId: vpc.id,
     tags: {
         Name: `${projectName}-private-rt-b-${environment}`,
@@ -134,18 +144,180 @@ const privateRouteTableB = new aws.ec2.RouteTable(`${projectName}-private-rt-b`,
 });
 
 // プライベートサブネットAとルートテーブルの関連付け
-const privateRtAssociationA = new aws.ec2.RouteTableAssociation(`${projectName}-private-rta-a`, {
+const privateRtAssociationA = new aws.ec2.RouteTableAssociation(`private-rta-a`, {
     subnetId: privateSubnetA.id,
     routeTableId: privateRouteTableA.id,
 });
 
 // プライベートサブネットBとルートテーブルの関連付け
-const privateRtAssociationB = new aws.ec2.RouteTableAssociation(`${projectName}-private-rta-b`, {
+const privateRtAssociationB = new aws.ec2.RouteTableAssociation(`private-rta-b`, {
     subnetId: privateSubnetB.id,
     routeTableId: privateRouteTableB.id,
 });
 
-// エクスポート
+// SSMパラメータストアへの保存
+// VPC情報をSSMに保存
+const vpcIdParam = new aws.ssm.Parameter(`vpc-id`, {
+    name: `${ssmPrefix}/network/vpc-id`,
+    type: "String",
+    value: vpc.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const vpcCidrOutputParam = new aws.ssm.Parameter(`vpc-cidr-output`, {
+    name: `${ssmPrefix}/network/vpc-cidr`,
+    type: "String",
+    value: vpc.cidrBlock,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+// サブネット情報をSSMに保存
+const publicSubnetAIdParam = new aws.ssm.Parameter(`public-subnet-a-id`, {
+    name: `${ssmPrefix}/network/public-subnet-a-id`,
+    type: "String",
+    value: publicSubnetA.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const publicSubnetBIdParam = new aws.ssm.Parameter(`public-subnet-b-id`, {
+    name: `${ssmPrefix}/network/public-subnet-b-id`,
+    type: "String",
+    value: publicSubnetB.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const privateSubnetAIdParam = new aws.ssm.Parameter(`private-subnet-a-id`, {
+    name: `${ssmPrefix}/network/private-subnet-a-id`,
+    type: "String",
+    value: privateSubnetA.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const privateSubnetBIdParam = new aws.ssm.Parameter(`private-subnet-b-id`, {
+    name: `${ssmPrefix}/network/private-subnet-b-id`,
+    type: "String",
+    value: privateSubnetB.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+// サブネットIDのリストをカンマ区切りで保存
+const publicSubnetIdsParam = new aws.ssm.Parameter(`public-subnet-ids`, {
+    name: `${ssmPrefix}/network/public-subnet-ids`,
+    type: "StringList",
+    value: pulumi.interpolate`${publicSubnetA.id},${publicSubnetB.id}`,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const privateSubnetIdsParam = new aws.ssm.Parameter(`private-subnet-ids`, {
+    name: `${ssmPrefix}/network/private-subnet-ids`,
+    type: "StringList",
+    value: pulumi.interpolate`${privateSubnetA.id},${privateSubnetB.id}`,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+// インターネットゲートウェイ情報をSSMに保官
+const igwIdParam = new aws.ssm.Parameter(`igw-id`, {
+    name: `${ssmPrefix}/network/igw-id`,
+    type: "String",
+    value: igw.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+// ルートテーブル情報をSSMに保存
+const publicRouteTableIdParam = new aws.ssm.Parameter(`public-rt-id`, {
+    name: `${ssmPrefix}/network/public-route-table-id`,
+    type: "String",
+    value: publicRouteTable.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const privateRouteTableAIdParam = new aws.ssm.Parameter(`private-rt-a-id`, {
+    name: `${ssmPrefix}/network/private-route-table-a-id`,
+    type: "String",
+    value: privateRouteTableA.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const privateRouteTableBIdParam = new aws.ssm.Parameter(`private-rt-b-id`, {
+    name: `${ssmPrefix}/network/private-route-table-b-id`,
+    type: "String",
+    value: privateRouteTableB.id,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+const privateRouteTableIdsParam = new aws.ssm.Parameter(`private-rt-ids`, {
+    name: `${ssmPrefix}/network/private-route-table-ids`,
+    type: "StringList",
+    value: pulumi.interpolate`${privateRouteTableA.id},${privateRouteTableB.id}`,
+    overwrite: true,
+    tags: {
+        Environment: environment,
+        ManagedBy: "pulumi",
+        Component: "network",
+    },
+});
+
+// エクスポート（既存のスタック参照用に残す）
 export const vpcId = vpc.id;
 export const vpcCidr = vpc.cidrBlock;
 export const publicSubnetIds = [publicSubnetA.id, publicSubnetB.id];
