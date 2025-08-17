f1574e4ed6063849d2af577802a4229ce3158073
YutoTakashi <yuto.takashi@plan-b.co.jp>
2025-08-18 08:58:11 +0900

    fix update public ip sh

--- bootstrap/scripts/update-public-ip.sh
+++ bootstrap/scripts/update-public-ip.sh
@@ -5,11 +5,20 @@
 
 set -e
 
+# IMDSv2対応: トークンを取得
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" -s)
+
 # Get the AWS region from instance metadata
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
+REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/region)
+
+# リージョンが取得できない場合はデフォルトを使用
+if [ -z "$REGION" ]; then
+    REGION="ap-northeast-1"
+    echo "Warning: Could not retrieve region from metadata, using default: $REGION"
+fi
 
 # Get the current public IP
-PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 # Check if public IP was retrieved
 if [ -z "$PUBLIC_IP" ] || [ "$PUBLIC_IP" == "None" ]; then
