3172d05332016d324b94120fb42b52bbddc1aebe
YutoTakashi <y.takashi@tielec.net>
2025-09-16 10:05:04 +0900

    [jenkins] add: Pulumi Stack ActionのAWS_REGIONパラメータにus-west-2を追加
    - infrastructure_pulumi_stack_action_job.groovy
    - infrastructure_pulumi_stack_action_test_job.groovy
    - マルチリージョン対応のためus-west-2を選択肢に追加

--- jenkins/jobs/dsl/infrastructure/infrastructure_pulumi_stack_action_test_job.groovy
+++ jenkins/jobs/dsl/infrastructure/infrastructure_pulumi_stack_action_test_job.groovy
@@ -41,7 +41,7 @@ pipelineJob(fullJobName) {
         stringParam('AWS_ACCESS_KEY_ID', '', 'AWS Access Key ID')
         nonStoredPasswordParam('AWS_SECRET_ACCESS_KEY', 'AWS Secret Access Key')
         nonStoredPasswordParam('AWS_SESSION_TOKEN', 'AWS Session Token（一時認証情報を使用する場合）')
-        choiceParam('AWS_REGION', ['ap-northeast-1'], 'AWSリージョン')        
+        choiceParam('AWS_REGION', ['ap-northeast-1', 'us-west-2'], 'AWSリージョン')        
         // === Pulumiプロジェクト設定 ===
         choiceParam('PULUMI_REPO_URL', ['https://github.com/tielec/infrastructure-as-code'], 'GitリポジトリURL')      
         stringParam('PULUMI_TARGET_BRANCH', 'main', 'チェックアウトするブランチ')
